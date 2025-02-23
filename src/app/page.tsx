9ed8279f6faf152212c3608cf3a7ff3756a5ade1
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-02-22 16:16:52 -0800

    recreate globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="bg-light-beige min-h-screen">
-      <main className="">
-      </main>
+        <div className="text-deep-pink">ももだよ</div>
     </div>
   );
 }
